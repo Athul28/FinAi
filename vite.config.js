b857587a72a5ebc7ac60d0e2b205e6d76a441f4f
Athul Bhandary <athulbhandary28@gmail.com>
2024-01-27 11:55:05 +0530

    4th commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default {
   plugins: [react()],
-})
+  build: {
+    target: 'es2015',
+    outDir: 'dist',
+    assetsDir: '.',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
+}
