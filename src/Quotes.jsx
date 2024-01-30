452b23e54520e3f0cde6d70eb9ca9ea25152b5a1
Athul Bhandary <athulbhandary28@gmail.com>
2024-01-30 22:52:13 +0530

    fixed typos

--- src/Quotes.jsx
+++ src/Quotes.jsx
@@ -3,11 +3,11 @@ import React from "react";
 function Quotes() {
   return (
     <div>
-      <blockquote class="border-l-4 border-gray-400 px-8 py-4 my-4 text-center" id="articles">
-        <p class="text-gray-700 font-extrabold text-3xl text-center mb-1">
+      <blockquote className="border-l-4 border-gray-400 px-8 py-4 my-4 text-center" id="articles">
+        <p className="text-gray-700 font-extrabold text-3xl text-center mb-1">
           "Never depend on a single income. Make an investment to create a second source"
         </p>
-        <cite class="text-gray-500 text-xl">- Warren Buffett</cite>
+        <cite className="text-gray-500 text-xl">- Warren Buffett</cite>
       </blockquote>
     </div>
   );
--- src/articles.jsx
+++ src/articles.jsx
@@ -9,20 +9,20 @@ function Articles() {
 
   return (
     <div>
-      <div class="grid grid-cols-3 gap-4 content-cente mx-5 my-4 text-white max-sm:grid-cols-1 text-center">
-        <div class="card">
+      <div className="grid grid-cols-3 gap-4 content-cente mx-5 my-4 text-white max-sm:grid-cols-1 text-center">
+        <div className="card">
           <img src={Book1} className="h-[250px] m-auto p-3" alt="psycology of money" />
-          <p class="">Summary of the psycology of money</p>
+          <p className="">Summary of the psycology of money</p>
           <button onClick={() => window.open(sitelink1)} className="card-button">Check it out</button>
         </div>
-        <div class="card">
+        <div className="card">
           <img src={Book2} className="h-[250px] m-auto p-3" alt="psycology of money" />
-          <p class="">Summary of Rich Dad Poor Dad</p>
+          <p className="">Summary of Rich Dad Poor Dad</p>
           <button onClick={() => window.open(sitelink2)} className="card-button">Check it out</button>
         </div>
-        <div class="card">
+        <div className="card">
           <img src={Book3} className="h-[250px] m-auto p-3" alt="psycology of money" />
-          <p class="">Summary of The Simple Path to Wealth</p>
+          <p className="">Summary of The Simple Path to Wealth</p>
           <button onClick={() => window.open(sitelink3)} className="card-button">Check it out</button>
         </div>
       </div>
