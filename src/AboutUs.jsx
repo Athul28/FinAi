c4356201c0a7846a18d8b284f282c6ba6fa523b2
Athul Bhandary <athulbhandary28@gmail.com>
2024-02-05 07:57:20 +0530

    Added team members name

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -3,12 +3,20 @@ import React from 'react';
 function AboutUs(){
     return(
         <div className="p-8 text-center text-[#222831]" id="aboutus">
-            <p className="font-bold text-xl mb-2">Invest Smarter, Reach Goals Faster. AI guides your money, you enjoy the journey</p>
-            <ul className='text-lg'>
+            <p className="font-bold text-xl mb-2 md:text-2xl">Invest Smarter, Reach Goals Faster. AI guides your money, you enjoy the journey</p>
+            <ul className='text-lg md:text-xl'>
                 <li>AI understands your income, expenses, and dreams</li>
                 <li>Get investment strategies, specific options, and clear steps</li>
                 <li>AI helps you invest, you control your future</li>
             </ul>
+            <hr className='border-[#222831] w-[100px] mx-auto border-dotted my-[25px] border-t-8' />
+            <div>
+                <p className='font-bold md:text-xl mb-2'>Our Team</p>
+                <p>Athul D Bhandary</p>
+                <p>Devam Mavani</p>
+                <p>Raksha Kamath B</p>
+                <p>Swathi R Kini</p>
+            </div>
         </div>
     );
 }
